838413e3bd331fdd4e7847e96c4c2785ed924b27
Stancu Mihai <mihai.stancu@nymphis.ro>
2022-04-08 21:07:10 +0300

    first commit

---
+++ DataAccess/SchoolDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=.;Database=SchoolDatabase;Trusted_Connection=True;");
+        }
+    }
+}
